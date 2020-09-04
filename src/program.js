29e8f644d437bb806f86003219b16650cbb32b98
Jordi Suijkerbuijk <j.suijkerbuijk@hotmail.com>
2020-09-05 01:06:15 +0200

    works
    works through copying from marnix try to make it cleaner and short as possible

--- src/program.js
+++ src/program.js
@@ -1,45 +1,77 @@
-const { readdirSync } = require('fs');
+const { readdirSync, lstatSync } = require('fs');
+const { join } = require('path');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
 const dlv = require('dlv');
 
-// let error = '';
+let _jsonResponse,
+  format = true;
+const result = {
+  directories: [],
+  entries: [],
+};
 
-function getGitDirectoriesFromPath(source) {
-  console.log('return readdirSync(source)', readdirSync(source));
-  // error = 'no path';
-  // return undefined;
+function isDirectory(source) {
+  return lstatSync(source).isDirectory();
 }
 
-// function isDirectory(source) {
-//   return lstatSync(source).isDirectory();
-// }
+function getDirectoriesFromPath(source) {
+  return readdirSync(source)
+    .map((name) => join(source, name))
+    .filter(isDirectory);
+}
 
-// function getGitDirectoriesFromPath(source) {
-//   return readdirSync(source)
-//     .map((name) => join(source, name))
-//     .filter(isDirectory);
-// }
+let counts = [0, 0];
 
-// async function getGitDirectory(items) {
-//   const terminalLine = await exec();
-// }
+async function getGitDirectory(directories, callback = () => {}) {
+  for (let i = 0; i < directories.length; i += 1) {
+    const directory = directories[i];
+    const appendForSplit = '|@|';
+    const f = `${format}${_jsonResponse ? appendForSplit : ''}`;
+    if (directory.match(/\.git/)) {
+      counts[0] += 1;
+      const { stdout, stderr } = await exec(
+        `cd ${directory}; git log  --pretty=format:"${f}" --no-merges --reverse`
+      );
+
+      const isValid = !stderr && stdout !== '';
+      counts[1] += 1;
+
+      if (_jsonResponse && isValid) {
+        result.directories.push(directory);
+        const regex = new RegExp(`\n|${appendForSplit}`, 'g');
+        result.entries.push(
+          ...stdout
+            .split(appendForSplit)
+            .map((output) => ({ commit: output.replace(regex, ''), directory }))
+            .filter((e) => e.commit)
+        );
+      }
+
+      if (counts[0] === counts[1]) {
+        callback();
+      }
+    }
+  }
+}
 
 async function program(program = {}, jsonResponse = true, cli = false) {
   // const day = dlv(program, 'day', '');
+  format = dlv(program, 'format', '%an <%ae> - %s');
+  _jsonResponse = program.json || jsonResponse;
   const path = dlv(program, 'path', '');
-  // console.log('path', path);
 
-  const directories = getGitDirectoriesFromPath(path);
-  console.log('directories', directories);
+  const directories = getDirectoriesFromPath(path);
 
-  // if (directories !== undefined) {
-  //   return new Promise((resolve) => {
-  //     // getGitDirectory(directories, () => {
-  //     //   if (cli) console.log(result);
-  //     //   return resolve(result);
-  //     // });
-  //   });
-  // }
-  return new Promise((resolve) => {});
+  if (_jsonResponse === false) return getGitDirectory(directories);
+
+  return new Promise((resolve) => {
+    getGitDirectory(directories, () => {
+      if (cli) console.log(result.entries);
+      return resolve(result);
+    });
+  });
+  // return new Promise((resolve) => {});
 }
 
 module.exports = program;
